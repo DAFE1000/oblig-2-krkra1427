0fd3df697bbef5110a963943de4a7384dccd8a21
krkra1427 <krkra1427@oslomet.no>
2026-03-07 16:12:10 +0000

    Funksjon inkl plot

--- innlevering2.py
+++ innlevering2.py
@@ -1,6 +1,19 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
 
 
+# Funksjonen f(x) og f´(x)
+def f(x):
+    return np.exp(-x/4) * np.arctan(x)
 
+def f_derivert(x):
+    pass
+
+
+
+# Plot av f(x)
+x = np.linspace(0, 10, 100)
+y = f(x)
+plt.plot(x, f(x))
+plt.grid()
+plt.show()
