f2f7d83cb9ea2d853a9ccdbb4ead7ac8653155cf
krkra1427 <krkra1427@oslomet.no>
2026-03-12 20:22:19 +0100

    Update innlevering2.py
    f(x) og f´(x)

--- innlevering2.py
+++ innlevering2.py
@@ -1,15 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+# Funksjonen f(x) og f´(x)
 # Funksjonen f(x) og f´(x)
 def f(x):
     return np.exp(-x/4) * np.arctan(x)
 
 def f_derivert(x):
+    return -np.exp(-x/4)/4 * (np.arctan(x) - (4 / (1 + x**2)))
+
+def halveringsmetode(a, b, n):
     pass
-
-
 
 # Plot av f(x)
 x = np.linspace(0, 20, 1000)
